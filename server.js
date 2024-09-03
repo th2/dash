ffb6cfcd0c3624708949165782fb21e2fc810ca4
Thorben Hehmann <th2@users.noreply.github.com>
2024-09-03 10:53:02 +0200

    Initial setup

---
+++ server.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const logger = require('./logger.js');
+const path = require('path');
+const fs = require('fs');
+const { count } = require('console');
+
+const services = fs.readdirSync(path.join(__dirname, '..')).filter(file => fs.statSync(path.join(__dirname, '..', file)).isDirectory());
+
+app.use(logger.visitReq());
+
+app.get('/', (req, res, next) => {
+    const timeStart = Date.now();
+    const visits = countVisits();
+    const timeEnd = Date.now();
+    const timeElapsed = timeEnd - timeStart;
+    
+    res.send({ visits, timeElapsed });
+});
+
+app.use((req, res) => {
+    res.send('');
+});
+
+app.use(logger.errorReq());
+
+http.listen(process.env.PORT, () => console.log(`listening on port ${process.env.PORT}`));
+
+function countVisits() {
+    let visitCount = {};
+    services.forEach(service => {
+        visitCount[service] = countVisitsPerService(service);
+    });
+    return visitCount;
+}
+
+function countVisitsPerService(service) {
+    makeSureLogDirExists(service);
+    let visitCount = 0;
+    fs.readdirSync(path.join(__dirname, '..', service, 'log', 'visit')).forEach(file => {
+        let fileAgeDays = (Date.now() - new Date(file.split('.')[0])) / 1000 / 60 / 60 / 24;
+        if (fileAgeDays < 7) {
+            fs.readFileSync(path.join(__dirname, '..', service, 'log', 'visit', file)).toString().split('\n').forEach(line => {
+                if (line) {
+                    visitCount++;
+                }
+            });
+        }
+    });
+    return visitCount;
+}
+
+function makeSureLogDirExists(service) {
+    fs.existsSync(path.join(__dirname, '..', service, 'log')) || fs.mkdirSync(path.join(__dirname, '..', service, 'log'));
+    fs.existsSync(path.join(__dirname, '..', service, 'log', 'visit')) || fs.mkdirSync(path.join(__dirname, '..', service, 'log', 'visit'));
+    fs.existsSync(path.join(__dirname, '..', service, 'log', 'error')) || fs.mkdirSync(path.join(__dirname, '..', service, 'log', 'error'));
+}
